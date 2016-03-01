bc6872af68d3f783ac43e8d5db226a335760310d
Student <Student@Mac-mini-3.local>
2016-02-29 17:04:55 -0700

    finished all problems

---
+++ main.js
@@ -0,0 +1,33 @@
+// Loop Practice 1
+// Problem 1
+//for (var i = 0; i<=500; i+=100){
+//   console.log(i);
+// }
+// Problem 2
+// for (var i = 1; i<=64; i*=2){
+//   console.log(i);
+// }
+// Problem 3
+// for (var i = 1; i <= 3; i++){
+//   for (var j = 1; j<=3; j++){
+//     console.log(i);
+//   }
+// }
+// Problem 4
+// for (var i = 0; i<=10; i+=2){
+//   console.log(i);
+// }
+// Problem 5
+// for (var i = 3; i<=15; i+=3){
+//   console.log(i);
+// }
+// Problem 6
+// for (var i = 9; i>=0; i--){
+//   console.log(i);
+// }
+// Problem 7
+// for (var i = 1; i < 4 ; i++){
+//   for (var j = 0; j < 4; j++){
+//       console.log(j);
+//   }
+// }
